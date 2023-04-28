bee1833cf79473de2392169c62fcb05287a78b90
Raul Yesid Rojas Ballesteros <131273447+YesidRojas06@users.noreply.github.com>
2023-04-28 07:04:00 -0500

    arreglos

--- Backend/models/administrador.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const FavoritosShechema=new mongoose.Schema({
-    usuario: {type:mongoose.Schema.ObjectId,ref:"Usuario",required:true},
-    pelicula:{type:mongoose.Schema.ObjectId,ref:"pelicula",required:true},
-    
-})
-export default mongoose.model("Favoritos",FavoritosShechema)
