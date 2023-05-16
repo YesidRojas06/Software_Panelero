dee30dd2cef69b637731fef92f21231f99c4b2e1
José Daniel Posada Martínez <posadamartinezjosedaniel@gmail.com>
2023-05-16 12:02:54 -0500

    actual9

---
+++ Backend/modelos/Labores.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+
+const schema= new mongoose.Schema({
+    nombreLabor:{type: String, required: true},
+    areaRealizar:{type: String, required: true},
+    pago:{type: Number, required: true},
+    acciones:{type: String, required: true},
+    estado:{type:Number,required:true}
+});
+
+
+const modeloLabor= mongoose.model("Labor",schema)
+
+export default modeloLabor
