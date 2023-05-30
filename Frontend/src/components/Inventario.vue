254257b81c9a25f7d5775781714203d07e7c86ba
José Daniel Posada Martínez <posadamartinezjosedaniel@gmail.com>
2023-05-30 07:55:59 -0500

    v

--- Frontend/src/components/Inventario.vue
+++ Frontend/src/components/Inventario.vue
@@ -1,4 +1,162 @@
 <template>
+  <q-layout>
+    <q-page-container>
+      <div class="title-container"> 
+        <h1 class="page-title">Mantenimiento</h1>
+      </div>
+
+        <div class="contenedor">
+          <div class="contenedor-conciertos">
+            <router-link class="lg4" to="/Bodegas">
+              <div class="card">
+                <div class="textos">
+                  <center><h6>Bodegas</h6></center>
+                  <!-- <q-separator style="background-color: #000000" dark inset /> -->
+                  <i class="material-icons" style="font-size: 100px;">store</i>
+                </div>
+              </div>
+            </router-link>
+
+            <router-link class="lg4" to="/Marcas">
+              <div class="card">
+                <div class="textos">
+                  <center><h6>Marcas</h6></center>
+                  <!-- <q-separator style="background-color: #000000" dark inset /> -->
+                  <i class="material-icons" style="font-size: 100px;">verified</i>
+                </div>
+              </div>
+            </router-link>
+
+            <router-link class="lg4" to="/Categorias">
+              <div class="card">
+                <div class="textos">
+                  <center><h6>Categorias</h6></center>
+                  <!-- <q-separator style="background-color: #000000" dark inset /> -->
+                  <i class="material-icons" style="font-size: 100px;">shopping_bag</i>
+                </div>
+              </div>
+            </router-link>
+
+            <router-link class="lg4" to="/Productos">
+              <div class="card" style="left: 405px;">
+                <div class="textos">
+                  <center><h6>Productos</h6></center>
+                  <!-- <q-separator style="background-color: #000000" dark inset /> -->
+                  <i class="material-icons" style="font-size: 100px;">inventory</i>
+                </div>
+              </div>
+            </router-link>
+
+          </div>
+          </div>
+
+              </q-page-container>
+          </q-layout>
+         
+  
+</template>
+
+<style>
+.lg4 {
+  text-decoration: none;
+
+}
+
+
+
+
+.contenedor {
+  width: 90%;
+  max-width: 1200px;
+  margin: auto;
+  padding: 40px 0;  
+  display: grid;
+  grid-template-columns: 3fr;
+  gap: 20px;
+}
+
+.contenedor-conciertos {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 10px;
+  margin-bottom: 5px;
+}
+
+.card {
+  border-radius: 10px;
+  min-height: 200px;
+  font-weight: bold;
+  padding: 20px;
+  position: relative;
+  overflow: hidden;
+  background-size: cover;
+  background-position: center center;
+  box-shadow: 0 1px 5px 0 rgba(0, 0, 0, 0.2);
+  border-radius: 3px;
+  background-color:white
+}
+
+.card:hover {
+  background-color: rgb(14, 224, 14);
+}
+
+.card .textos {
+  height: 100%;
+  color: #000000;
+  display: flex;
+  flex-direction: column-reverse;
+  justify-content: space-between;
+}
+
+@media screen and (max-width: 900px) {
+  .contenedor-conciertos {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 20px;
+  }
+}
+
+@media screen and (max-width: 700px) {
+  .contenedor-conciertos {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 20px;
+  }
+}
+
+@media screen and (max-width: 600px) {
+  .contenedor-conciertos {
+    display: grid;
+    grid-template-columns: repeat(1, 1fr);
+    gap: 20px;
+  }
+}
+
+
+.title-container {
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  margin-top: -60px;
+
+}
+
+.page-title {
+  text-align: center;
+  font-size: 50px; /* Ajusta el tamaño de la letra según tus preferencias */
+  border-bottom: 1px solid black; /* Agrega una línea sólida de 1px de grosor y color negro */
+  padding-bottom: 10px; /* Ajusta el espacio entre el texto y la línea */
+  border-bottom: 3px solid black; /* Ajusta el grosor de la línea según tus preferencias */
+  width: 10000px; /* Ajusta la anchura del contenedor del título */
+}
+
+
+
+</style>
+
+
+
+<!-- <template>
   
 
     <q-layout>
@@ -178,4 +336,4 @@
   
   
   
-  </style>
+  </style> -->
